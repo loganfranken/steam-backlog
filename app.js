39c9c16fae0e496541eec22e7e68ad694ec99a39
Logan Franken <loganfranken@gmail.com>
2015-06-18 21:40:47 -0700

    Added logic to exclude games based on config

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
+require('./es6-array-include')();
+
 var steam = require('./steam');
 var hltb = require('./hltb');
+
 var config = require('./config');
 
 steam.getSteamGameListByUsername(config.apiKey, config.username,
 
   function(gameList) {
 
+    gameList.forEach(function(game, i) {
+
+      if(config.exclude.includes(game.name)) {
+        return;
+      }
+
+      console.log(game.name);
+
+    });
+
+    /*
     var smallGameList = gameList.slice(0, 5);
     smallGameList.forEach(function(game, i) {
 
@@ -14,6 +28,7 @@ steam.getSteamGameListByUsername(config.apiKey, config.username,
       });
 
     });
+    */
 
   }
 
--- config.example.js
+++ config.example.js
@@ -1,5 +1,6 @@
 var config = {};
 config.apiKey = 'YOUR_API_KEY';
 config.username = 'YOUR_USERNAME';
+config.exclude = ['GAME_NAME_1', 'GAME_NAME_2'];
 
 module.exports = config;
